a198fc9b85306f13827e616e302b142200eeb870
chafin123 <stevend.chafin@gmail.com>
2021-02-08 09:06:03 -0600

    removed uneeded code

--- Src/script.js
+++ Src/script.js
@@ -1,15 +1,5 @@
 let firstName = document.getElementById("first-name");
 let lastName = document.getElementById("last-name");
-
-/*
-const nameVisibile = () => {
-firstName.style.display ="block";
-firstName.style.width = "50%";
-lastName.style.display = "block";
-lastName.style.width = "50%"
-}
-document.body.addEventListener("load", nameVisibile());
-*/
 document.getElementById("certs").onclick = function() {
     document.getElementById("certs").classList.toggle('show');
 }
