20f4cf3655dc0a5c2aedd4a01c2b1c72ebcf257f
chafin123 <stevend.chafin@gmail.com>
2021-02-23 16:42:46 -0600

    Fixed problems got rid of unsused js file

--- Src/script.js
+++
@@ -1,5 +0,0 @@
-let firstName = document.getElementById("first-name");
-let lastName = document.getElementById("last-name");
-document.getElementById("certs").onclick = function() {
-    document.getElementById("certs").classList.toggle('show');
-}
